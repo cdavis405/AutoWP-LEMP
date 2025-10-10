1d6b055f5de7257adcfb20c8cffef64126121162
cdavis405 <chisdavis@gmail.com>
2025-10-10 04:59:51 +0000

    Clear repository

--- wp-content/themes/retaguide/assets/js/editor.js
+++
@@ -1,1 +0,0 @@
-(function(wp){const {domReady, editPost}=wp;domReady(function(){editPost.__experimentalSetPreviewDeviceType&&editPost.__experimentalSetPreviewDeviceType('Desktop');});})(window.wp||{});
